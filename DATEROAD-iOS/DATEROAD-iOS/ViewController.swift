316a2859ec4255f1d1f457554e1b2d3dd6f2a3a6
seuriseuljjeok <heeseul09@naver.com>
2024-06-24 21:55:51 +0900

    [Setting] 스토리보드 제거 (#1)

---
+++ DATEROAD-iOS/DATEROAD-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DATEROAD-iOS
+//
+//  Created by 윤희슬 on 6/24/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
