16d2e59ad056f2dff872ef3323b9b6bae830f42b
seuriseuljjeok <heeseul09@naver.com>
2024-07-01 16:19:29 +0900

    [Feat] 카카오 & 애플 로그인 구현 (#7)

--- DATEROAD-iOS/DATEROAD-iOS/Presentation/Login/ViewModels/LoginViewModel.swift
+++ DATEROAD-iOS/DATEROAD-iOS/Presentation/Login/ViewModels/LoginViewModel.swift
@@ -6,3 +6,68 @@
 //
 
 import Foundation
+
+import KakaoSDKAuth
+import KakaoSDKCommon
+import KakaoSDKUser
+
+final class LoginViewModel {
+    
+    var isKaKaoLogin: ObservablePattern<Bool> = ObservablePattern(nil)
+    
+    var socialType: ObservablePattern<SocialType> = ObservablePattern(nil)
+    
+    var socialToken: ObservablePattern<String> = ObservablePattern(nil)
+    
+    var onLoginSuccess: (() -> Void)?
+    
+    var onLoginFailure: ((Error) -> Void)?
+}
+
+extension LoginViewModel {
+    
+    func checkKakaoInstallation(completion: @escaping (Bool) -> Void) {
+        if UserApi.isKakaoTalkLoginAvailable() {
+            self.socialType.value = .kakao
+            self.isKaKaoLogin.value = true
+            completion(true)
+        } else {
+            completion(false)
+        }
+    }
+    
+    func loginWithKakaoApp() {
+        UserApi.shared.loginWithKakaoTalk { [weak self] (oauthToken, error) in
+            self?.handleKakaoLoginResult(oauthToken: oauthToken, error: error)
+        }
+    }
+    
+    func loginWithKakaoWeb() {
+        UserApi.shared.loginWithKakaoAccount { [weak self] (oauthToken, error) in
+            self?.handleKakaoLoginResult(oauthToken: oauthToken, error: error)
+        }
+    }
+    
+    func setToken(token: String) {
+        guard let key = self.socialType.value?.rawValue else { return }
+        guard let value = self.isKaKaoLogin.value else { return }
+        UserDefaults.standard.setValue(value, forKey: key)
+        self.socialToken.value = token
+        self.onLoginSuccess?()
+    }
+    
+    func handleKakaoLoginResult(oauthToken: OAuthToken?, error: Error?) {
+        if let error {
+            print("로그인 실패: \(error.localizedDescription)")
+            self.onLoginFailure?(error)
+        } else {
+            print("로그인 성공: \(String(describing: oauthToken))")
+            guard let oauthToken = oauthToken else { return }
+            UserApi.shared.me { (user, error) in
+                print("user identifier : \(String(describing: user?.kakaoAccount?.profile?.nickname))")
+            }
+            setToken(token: oauthToken.accessToken)
+        }
+    }
+    
+}
