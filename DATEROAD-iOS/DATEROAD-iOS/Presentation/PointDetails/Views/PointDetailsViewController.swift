2942e878562470f7c14d1a91c948aa7751c1229a
cirtuare <cirtuare@gmail.com>
2024-07-03 01:47:28 +0900

    [Add] PointDetailsVC 추가 (#15)

---
+++ DATEROAD-iOS/DATEROAD-iOS/Presentation/PointDetails/Views/PointDetailsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PointDetailsViewController.swift
+//  DATEROAD-iOS
+//
+//  Created by 이수민 on 7/3/24.
+//
+
+import UIKit
+
+class PointDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
