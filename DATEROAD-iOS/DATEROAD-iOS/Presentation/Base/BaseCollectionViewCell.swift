fda08ff745283f790fccfb4474c023db61e0e1c8
seuriseuljjeok <heeseul09@naver.com>
2024-07-04 06:30:41 +0900

    [Add] BaseCollectionViewCell 추가 (#17)

--- DATEROAD-iOS/DATEROAD-iOS/Presentation/Base/BaseCollectionViewCell.swift
+++ DATEROAD-iOS/DATEROAD-iOS/Presentation/Base/BaseCollectionViewCell.swift
@@ -7,6 +7,36 @@
 
 import UIKit
 
+import SnapKit
+import Then
+
 class BaseCollectionViewCell: UICollectionViewCell {
+    
+    // MARK: - Properties
+    
+    static let cellIdentifier = String(describing: BaseCollectionViewCell.self)
+    
+    
+    // MARK: - Life Cycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setHierarchy()
+        setLayout()
+        setStyle()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setHierarchy() {}
+    
+    func setLayout() {}
+    
+    func setStyle() {
+        self.backgroundColor = UIColor(resource: .drWhite)
+    }
     
 }
