089786967b2fc35764600ba447c759094b4f2fa1
seuriseuljjeok <heeseul09@naver.com>
2024-07-04 06:31:14 +0900

    [Add] 공용 버튼 컴포넌트 분리 (#17)

--- DATEROAD-iOS/DATEROAD-iOS/Global/Literals/StringLiterals.swift
+++ DATEROAD-iOS/DATEROAD-iOS/Global/Literals/StringLiterals.swift
@@ -20,4 +20,20 @@ enum StringLiterals {
         static let appleLoginLabel = "Apple ID로 로그인"
         static let privacyPolicyLabel = "개인정보처리방침"
     }
+    
+    enum Onboarding {
+        static let next = "다음"
+        static let createProfile = "프로필 생성하기"
+        static let firstMainInfoLabel = "데이트로드는 포인트로\n데이트 코스를 열람할 수 있어요."
+        static let firstSubInfoLabel = "최초 3회 찬스로 다른 사람의 데이트 코스를\n구경해보세요"
+        static let secondMainInfoLabel = "데이트 코스를 등록하면\n100 포인트를 얻을 수 있어요"
+        static let secondSubInfoLabel = "내 연인과 함께한 데이트 코스를 자랑하고\n포인트를 받아보세요"
+        static let thirdMainInfoLabel = "쌓인 포인트로\n다양한 데이트 코스를 둘러보세요"
+        static let thirdSubInfoLabel = "모인 포인트는 데이트 장소를 예약할 때\n현금처럼 사용 가능해요"
+        static let dateCourse = "데이트 코스"
+        static let firstMainPoint = "포인트"
+        static let secondMainPoint = "100 포인트"
+        static let thirdMainPoint = "다양한 데이트 코스"
+
+    }
 }
--- DATEROAD-iOS/DATEROAD-iOS/Global/Protocols/DRButton.swift
+++ DATEROAD-iOS/DATEROAD-iOS/Global/Protocols/DRButton.swift
@@ -7,10 +7,75 @@
 
 import UIKit
 
-import SnapKit
-import Then
+protocol DRButton {
 
-class DRButton: UIButton {
+    var bgColor: UIColor { get }
     
-    enum
+    var fontColor: UIColor { get }
+        
+    var borderWidth: CGFloat { get }
+        
+    var isEnabled: Bool { get }
+        
 }
+
+struct NextButton: DRButton {
+    
+    var bgColor: UIColor =  UIColor(resource: .drWhite)
+    
+    var fontColor: UIColor = UIColor(resource: .deepPurple)
+    
+    var borderWidth: CGFloat = 1
+
+    var isEnabled: Bool = true
+
+}
+
+struct EnabledButton: DRButton {
+    
+    var bgColor: UIColor = UIColor(resource: .deepPurple)
+    
+    var fontColor: UIColor = UIColor(resource: .drWhite)
+    
+    var borderWidth: CGFloat = 0
+
+    var isEnabled: Bool = true
+                    
+}
+
+struct DisabledButton: DRButton {
+    
+    var bgColor: UIColor = UIColor(resource: .gray200)
+    
+    var fontColor: UIColor = UIColor(resource: .gray400)
+    
+    var borderWidth: CGFloat = 0
+        
+    var isEnabled: Bool = false
+        
+}
+
+struct KakaoLoginButton: DRButton {
+    
+    var bgColor: UIColor = UIColor(resource: .drWhite)
+    
+    var fontColor: UIColor = UIColor(resource: .drBlack)
+    
+    var borderWidth: CGFloat = 0
+        
+    var isEnabled: Bool = true
+        
+}
+
+struct AppleLoginButton: DRButton {
+    
+    var bgColor: UIColor = UIColor(resource: .drBlack)
+    
+    var fontColor: UIColor = UIColor(resource: .drWhite)
+    
+    var borderWidth: CGFloat = 0
+        
+    var isEnabled: Bool = true
+        
+}
+
