a030ee3e0e9d42f2202cd617988c9a9ad22b2cc5
cirtuare <cirtuare@gmail.com>
2024-07-05 04:01:03 +0900

    [Add] 폴더링 세팅 및 뷰컨 추가 (#23)

---
+++ DATEROAD-iOS/DATEROAD-iOS/Presentation/ViewedCourse/Views/ViewedCourseViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewedCourseViewController.swift
+//  DATEROAD-iOS
+//
+//  Created by 이수민 on 7/5/24.
+//
+
+import UIKit
+
+class ViewedCourseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
