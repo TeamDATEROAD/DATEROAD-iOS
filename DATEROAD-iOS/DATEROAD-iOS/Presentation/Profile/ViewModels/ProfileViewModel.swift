990c0568a92872ea673bfe849ab8102275fa9f33
seuriseuljjeok <heeseul09@naver.com>
2024-07-05 22:39:59 +0900

    [Feat] 뷰모델 추가 (#22)

--- DATEROAD-iOS/DATEROAD-iOS/Presentation/Profile/ViewModels/ProfileViewModel.swift
+++ DATEROAD-iOS/DATEROAD-iOS/Presentation/Profile/ViewModels/ProfileViewModel.swift
@@ -9,7 +9,19 @@ import Foundation
 
 final class ProfileViewModel {
     
+    // TODO: - 중복 확인 로직 추가 예정
+
     var tagData: [String] = []
+    
+    var nickname: ObservablePattern<String> = ObservablePattern("")
+    
+    var tagCount: ObservablePattern<Int> = ObservablePattern(0)
+        
+    var onValidNickname: ((Bool) -> Void)?
+    
+    var onValidSelect: ((Bool) -> Void)?
+    
+    var onSuccessRegister: ((Bool) -> Void)?
  
     init() {
         fetchTagData()
@@ -21,5 +33,42 @@ extension ProfileViewModel {
     
     func fetchTagData() {
         tagData = TendencyTag.allCases.map { $0.tagTitle }
+    }
+    
+    
+    func checkValidNickname() {
+        let nickname = self.nickname.value ?? ""
+        if nickname.isEmpty {
+            self.onValidNickname?(false)
+        } else {
+            // TODO: - 닉네임이 비어있지 않은 경우 중복 확인 처리 로직 추가 예정
+            self.onValidNickname?(true)
+        }
+    }
+    
+    func countSelectedTag(isSelected: Bool) {
+        let oldCount = tagCount.value ?? 0
+        
+        if isSelected {
+            tagCount.value = oldCount + 1
+        } else {
+            tagCount.value = oldCount - 1
+        }
+        
+        checkTagCount()
+    }
+    
+    func checkTagCount() {
+        guard let count = tagCount.value else { return }
+
+        if count >= 1 && count <= 3 {
+            self.onValidSelect?(true)
+        } else {
+            self.onValidSelect?(false)
+        }
+    }
+    
+    func checkValidRegistration() {
+        
     }
 }
