fc1a9f5b5821e847dc2243f4b3508538ccbf4584
seuriseuljjeok <heeseul09@naver.com>
2024-07-09 05:17:35 +0900

    [Feat] 포인트 제도 소개 UI 구현 (#37)

--- DATEROAD-iOS/DATEROAD-iOS/Presentation/PointSystem/Views/Cells/PointSystemCollectionViewCell.swift
+++ DATEROAD-iOS/DATEROAD-iOS/Presentation/PointSystem/Views/Cells/PointSystemCollectionViewCell.swift
@@ -5,4 +5,76 @@
 //  Created by 윤희슬 on 7/9/24.
 //
 
-import Foundation
+import UIKit
+
+final class PointSystemCollectionViewCell: BaseCollectionViewCell {
+    
+    // MARK: - UI Properties
+    
+    private let illustrationView: UIImageView = UIImageView()
+    
+    private let mainLabel: UILabel = UILabel()
+    
+    private let subLabel: UILabel = UILabel()
+    
+    override func setHierarchy() {
+        self.addSubviews(illustrationView, mainLabel, subLabel)
+    }
+    
+    override func setLayout() {
+
+        illustrationView.snp.makeConstraints {
+            $0.verticalEdges.equalToSuperview().inset(14)
+            $0.leading.equalToSuperview().inset(12)
+            $0.size.equalTo(70)
+        }
+        
+        mainLabel.snp.makeConstraints {
+            $0.top.trailing.equalToSuperview().inset(14)
+            $0.leading.equalTo(illustrationView.snp.trailing).offset(15)
+        }
+        
+        subLabel.snp.makeConstraints {
+            $0.bottom.trailing.equalToSuperview().inset(14)
+            $0.leading.equalTo(illustrationView.snp.trailing).offset(15)
+        }
+    }
+    
+    // TODO: - 이미지 바인딩 수정 예정
+    
+    override func setStyle() {
+        self.do {
+            $0.backgroundColor = UIColor(resource: .gray100)
+            $0.clipsToBounds = true
+            $0.layer.cornerRadius = 14
+        }
+        
+        illustrationView.do {
+            $0.clipsToBounds = true
+            $0.image = UIImage(resource: .emptyProfileImg)
+            $0.layer.cornerRadius = 35
+        }
+        
+        mainLabel.do {
+            $0.backgroundColor = UIColor(resource: .gray100)
+            $0.setAttributedText(fullText: <#T##String#>, pointText: <#T##String#>, pointColor: <#T##UIColor#>, lineHeight: <#T##CGFloat#>)
+            $0.setLabel(alignment: .left, textColor: UIColor(resource: .drBlack), font: UIFont.suit(.body_bold_15))
+            $0.numberOfLines = 2
+        }
+        
+        subLabel.do {
+            $0.backgroundColor = UIColor(resource: .gray100)
+            $0.setLabel(alignment: .left, textColor: UIColor(resource: .gray500), font: UIFont.suit(.body_med_13))
+            $0.numberOfLines = 1
+        }
+    }
+    
+}
+
+extension PointSystemCollectionViewCell {
+    
+    func bindData(mainText: String, subText: String) {
+        self.mainLabel.text = mainText
+        self.subLabel.text = subText
+    }
+}
--- DATEROAD-iOS/DATEROAD-iOS/Presentation/PointSystem/Views/PointSystemView.swift
+++ DATEROAD-iOS/DATEROAD-iOS/Presentation/PointSystem/Views/PointSystemView.swift
@@ -5,4 +5,70 @@
 //  Created by 윤희슬 on 7/9/24.
 //
 
-import Foundation
+import UIKit
+
+final class PointSystemView: BaseView {
+    
+    // MARK: - UI Properties
+    
+    private let mainLabel: UILabel = UILabel()
+
+    private let subLabel: UILabel = UILabel()
+
+    let pointSystemCollectionView: UICollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+
+    
+    // MARK: - Properties
+    
+    // MARK: - Life Cycle
+    
+    override func setHierarchy() {
+        self.addSubviews(mainLabel, subLabel, pointSystemCollectionView)
+    }
+    
+    override func setLayout() {
+        mainLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(40)
+            $0.horizontalEdges.equalToSuperview().inset(16)
+        }
+        
+        subLabel.snp.makeConstraints {
+            $0.top.equalTo(mainLabel.snp.bottom).offset(14)
+            $0.horizontalEdges.equalToSuperview().inset(16)
+            
+        }
+        
+        pointSystemCollectionView.snp.makeConstraints {
+            $0.top.equalTo(subLabel.snp.bottom).offset(65)
+            $0.horizontalEdges.equalToSuperview().inset(16)
+            $0.bottom.equalToSuperview().inset(30)
+        }
+    }
+    
+    override func setStyle() {
+        mainLabel.do {
+            $0.textAlignment = .left
+            $0.numberOfLines = 2
+            $0.font = UIFont.suit(.title_extra_20)
+            $0.setAttributedText(fullText: StringLiterals.Onboarding.firstMainInfoLabel,
+                                 pointText: StringLiterals.Onboarding.firstMainPoint,
+                                 pointColor: UIColor(resource: .deepPurple),
+                                 lineHeight: 1.04)
+        }
+        
+        subLabel.do {
+            $0.numberOfLines = 0
+            $0.setLabel(text: StringLiterals.PointSystem.subTitle,
+                        alignment: .left,
+                        textColor: UIColor(resource: .gray500),
+                        font: UIFont.suit(.body_med_15) )
+        }
+        
+        pointSystemCollectionView.do {
+            let layout = UICollectionViewFlowLayout()
+            layout.scrollDirection = .vertical
+            $0.collectionViewLayout = layout
+            $0.backgroundColor = UIColor(resource: .drWhite)
+        }
+    }
+}
