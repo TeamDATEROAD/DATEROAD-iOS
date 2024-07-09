798b7bb909a59b9fa1e05719097b1c0e1a5e87dd
kms0233 <cnblue789@naver.com>
2024-07-10 02:28:00 +0900

    [Chore] 기본 폴더 세팅 (#41)

---
+++ DATEROAD-iOS/DATEROAD-iOS/Presentation/Course/Models/CourseModel.swift
@@ -0,0 +1,8 @@
+//
+//  CourseModel.swift
+//  DATEROAD-iOS
+//
+//  Created by 김민서 on 7/10/24.
+//
+
+import Foundation
---
+++ DATEROAD-iOS/DATEROAD-iOS/Presentation/Course/ViewModels/CourseViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CourseViewModel.swift
+//  DATEROAD-iOS
+//
+//  Created by 김민서 on 7/10/24.
+//
+
+import Foundation
