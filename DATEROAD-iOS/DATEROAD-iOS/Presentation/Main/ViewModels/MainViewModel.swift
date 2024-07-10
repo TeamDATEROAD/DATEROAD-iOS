87d1501ebc4db90157347e104112a6568f192a28
seuriseuljjeok <heeseul09@naver.com>
2024-07-11 06:31:13 +0900

    [Feat] 메인 뷰모델 구현 (#46)

--- DATEROAD-iOS/DATEROAD-iOS/Presentation/Main/ViewModels/MainViewModel.swift
+++ DATEROAD-iOS/DATEROAD-iOS/Presentation/Main/ViewModels/MainViewModel.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+
+final class MainViewModel {
+    
+    var nickname: ObservablePattern<String> = ObservablePattern(nil)
+    
+    var upcomingData: ObservablePattern<UpcomingDateModel> = ObservablePattern(nil)
+    
+    var mainUserData: ObservablePattern<MainUserModel> = ObservablePattern(nil)
+
+    var hotCourseData: ObservablePattern<[DateCourseModel]> = ObservablePattern(nil)
+    
+    var bannerData: ObservablePattern<[BannerModel]> = ObservablePattern(nil)
+
+    var newCourseData: ObservablePattern<[DateCourseModel]> = ObservablePattern(nil)
+
+    let sectionData: [MainSection] = MainSection.dataSource
+    
+    
+    init() {
+        fetchSectionData()
+    }
+    
+}
+
+extension MainViewModel {
+    
+    // TODO: - 서버 통신 후 수정 예정
+    
+    func fetchSectionData() {
+        upcomingData.value = UpcomingDateModel.dummyData
+        mainUserData.value = MainUserModel.dummyData
+        hotCourseData.value = DateCourseModel.hotDateDummyData
+        newCourseData.value = DateCourseModel.newDateDummyData
+        bannerData.value = BannerModel.bannerDummyData
+    }
+}
