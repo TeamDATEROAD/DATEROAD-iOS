543d98ba0e109b507524960bfd433779b5d241ea
cirtuare <cirtuare@gmail.com>
2024-07-10 22:49:28 +0900

    [Add] 파일 기본 세팅 (#47)

---
+++ DATEROAD-iOS/DATEROAD-iOS/Global/UIComponents/CustomAlert/CustomAlertViewController.swift
@@ -0,0 +1,14 @@
+//
+//  CustomAlertViewController.swift
+//  DATEROAD-iOS
+//
+//  Created by 이수민 on 7/10/24.
+//
+
+import UIKit
+
+class CustomAlertViewController: BaseViewController {
+
+    
+
+}
