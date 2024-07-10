75123c87e7ae2a37950a58164d87d935424b6e52
seuriseuljjeok <heeseul09@naver.com>
2024-07-11 06:33:09 +0900

    [Feat] 컴포지셔널 레이아웃 프로토콜 구현 (#46)

--- DATEROAD-iOS/DATEROAD-iOS/Global/Protocols/MainSectionLayout.swift
+++ DATEROAD-iOS/DATEROAD-iOS/Global/Protocols/MainSectionLayout.swift
@@ -5,4 +5,96 @@
 //  Created by 윤희슬 on 7/10/24.
 //
 
-import Foundation
+import UIKit
+
+protocol MainSectionLayout {
+    
+    var itemSize: NSCollectionLayoutSize { get }
+    
+    var groupSize: NSCollectionLayoutSize { get }
+        
+    var itemContentInset: NSDirectionalEdgeInsets { get }
+    
+    var groupContentInset: NSDirectionalEdgeInsets { get }
+
+    var sectionContentInset: NSDirectionalEdgeInsets { get }
+    
+    var elementKind: String? { get }
+    
+    var supplemetaryItemSize: NSCollectionLayoutSize { get }
+
+    var supplementaryAlignment: NSRectAlignment { get }
+    
+}
+
+extension MainSectionLayout {
+    var itemSize: NSCollectionLayoutSize {
+        return NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
+    }
+    
+    var itemContentInset: NSDirectionalEdgeInsets {
+        return NSDirectionalEdgeInsets.zero
+    }
+    
+    var groupContentInset: NSDirectionalEdgeInsets {
+        return NSDirectionalEdgeInsets.zero
+    }
+    
+    var sectionContentInset: NSDirectionalEdgeInsets {
+        return NSDirectionalEdgeInsets(top: 0, leading: 16, bottom: 0, trailing: 16)
+    }
+    
+    var elementKind: String? {
+        return nil
+    }
+    
+    var supplemetaryItemSize: NSCollectionLayoutSize {
+        return NSCollectionLayoutSize(widthDimension: .fractionalWidth(0), heightDimension: .estimated(0))
+    }
+    
+    var supplementaryAlignment: NSRectAlignment {
+        return .top
+    }
+}
+
+struct UpcomingDateLayout: MainSectionLayout {
+    
+    var groupSize: NSCollectionLayoutSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(343/375), heightDimension: .fractionalHeight(255 / 812))
+            
+    var sectionContentInset: NSDirectionalEdgeInsets =  NSDirectionalEdgeInsets(top: 0, leading: 16, bottom: 36, trailing: 16)
+    
+}
+
+struct HotDateLayout: MainSectionLayout {
+    
+    var groupSize: NSCollectionLayoutSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(255 / 812))
+    
+    var groupContentInset: NSDirectionalEdgeInsets =  NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 16)
+    
+    var sectionContentInset: NSDirectionalEdgeInsets =  NSDirectionalEdgeInsets(top: 21, leading: 16, bottom: 30, trailing: 0)
+    
+//    var elementKind: String? = HotDateCourseHeader.elementKind
+    
+    var supplemetaryItemSize: NSCollectionLayoutSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(86/812))
+    
+}
+
+struct BannerDateLayout: MainSectionLayout {
+    
+    var groupSize: NSCollectionLayoutSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(132 / 812))
+
+}
+
+struct NewDateLayout: MainSectionLayout {
+    
+    var groupSize: NSCollectionLayoutSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(255 / 812))
+    
+    var groupContentInset: NSDirectionalEdgeInsets =  NSDirectionalEdgeInsets(top: 10, leading: 0, bottom: 10, trailing: 0)
+    
+    var sectionContentInset: NSDirectionalEdgeInsets =  NSDirectionalEdgeInsets(top: 30, leading: 16, bottom: 16, trailing: 16)
+    
+//    var elementKind: String? = NewDateCourseHeader.elementKind
+    
+    var supplemetaryItemSize: NSCollectionLayoutSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(50/812))
+    
+}
