a4b59bbbce1e229464d05595ff754df6f39184b1
seuriseuljjeok <heeseul09@naver.com>
2024-07-11 06:29:20 +0900

    [Add] 패딩 라벨 컴포넌트 추가 (#46)

--- DATEROAD-iOS/DATEROAD-iOS/Global/UIComponents/DRPaddingLabel.swift
+++ DATEROAD-iOS/DATEROAD-iOS/Global/UIComponents/DRPaddingLabel.swift
@@ -5,4 +5,25 @@
 //  Created by 윤희슬 on 7/11/24.
 //
 
-import Foundation
+import UIKit
+
+class DRPaddingLabel: UILabel {
+    var padding = UIEdgeInsets.zero
+
+    override func drawText(in rect: CGRect) {
+        let insets = padding
+        super.drawText(in: rect.inset(by: insets))
+    }
+
+    override var intrinsicContentSize: CGSize {
+        let size = super.intrinsicContentSize
+        return CGSize(width: size.width + padding.left + padding.right,
+                      height: size.height + padding.top + padding.bottom)
+    }
+
+    func setPadding(top: CGFloat, left: CGFloat, bottom: CGFloat, right: CGFloat) {
+        self.padding = UIEdgeInsets(top: top, left: left, bottom: bottom, right: right)
+        setNeedsDisplay()
+    }
+}
+
