75a0881c042111d6a7dcae4a247d7f16d876dfef
seuriseuljjeok <heeseul09@naver.com>
2024-07-15 04:23:19 +0900

    [Setting] BaseTargetType 세팅 (#67)

--- DATEROAD-iOS/DATEROAD-iOS/Network/Base/BaseTargetType.swift
+++ DATEROAD-iOS/DATEROAD-iOS/Network/Base/BaseTargetType.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+import Moya
+
+protocol BaseTargetType: TargetType {
+    var utilPath: String { get }
+    var method: Moya.Method { get }
+}
+
+extension BaseTargetType {
+    
+    var baseURL: URL {
+        guard let urlString = Bundle.main.object(forInfoDictionaryKey: Config.Keys.Plist.baseURL) as? String,
+              let url = URL(string: urlString) else {
+            fatalError("🚨BASE_URL을 찾을 수 없습니다🚨")
+        }
+        return url
+    }
+    
+    var headers: [String : String]? {
+        let headers = ["Content-Type" : "application/json"]
+        return headers
+    }
+    
+}
