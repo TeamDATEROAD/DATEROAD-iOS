a1b22da73914140544ac118832d1b1afa1a57adc
seuriseuljjeok <heeseul09@naver.com>
2024-07-16 05:04:55 +0900

    [Feat] 유저 프로필 가져오기 서비스 구현 (#74)

--- DATEROAD-iOS/DATEROAD-iOS/Network/Main/MainService.swift
+++ DATEROAD-iOS/DATEROAD-iOS/Network/Main/MainService.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+import Moya
+
+protocol MainServiceProtocol {
+    func getUserProfile(
+                        completion: @escaping (NetworkResult<GetUserProfileResponse>) -> ())
+}
+
+final class MainService: BaseService, MainServiceProtocol {
+    
+    let provider = MoyaProvider<MainTargetType>(plugins: [MoyaLoggingPlugin()])
+
+    func getUserProfile(completion: @escaping (NetworkResult<GetUserProfileResponse>) -> ()) {
+        provider.request(.getUserProfile) { result in
+            switch result {
+            case .success(let response):
+                let networkResult: NetworkResult<GetUserProfileResponse> = self.judgeStatus(statusCode: response.statusCode, data: response.data) 
+                completion(networkResult)
+            case .failure(let err):
+                print(err)
+            }
+        }
+    }
+}
