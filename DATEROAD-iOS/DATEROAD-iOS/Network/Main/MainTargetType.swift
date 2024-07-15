6f2f7a95383bfb3e25a06dfe9bdae2a89134c2cb
seuriseuljjeok <heeseul09@naver.com>
2024-07-16 05:09:33 +0900

    [Feat] MainTargetType 구현 (#74)

--- DATEROAD-iOS/DATEROAD-iOS/Network/Main/MainTargetType.swift
+++ DATEROAD-iOS/DATEROAD-iOS/Network/Main/MainTargetType.swift
@@ -6,3 +6,63 @@
 //
 
 import Foundation
+
+import Moya
+
+enum MainTargetType {
+    case getUserProfile
+    case getFilteredDateCourse(sortBy: String)
+    case getBanner
+    case getUpcomingDate
+}
+
+extension MainTargetType: BaseTargetType {
+    
+    var utilPath: String {
+        return "api/v1/"
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .getUserProfile, .getFilteredDateCourse, .getBanner, .getUpcomingDate:
+            return .get
+        }
+    }
+    
+    var path: String {
+        switch self {
+        case .getUserProfile:
+            return utilPath + "users/main"
+        case .getFilteredDateCourse:
+            return utilPath + "courses/sort"
+        case .getBanner:
+            return utilPath + "advertisements"
+        case .getUpcomingDate:
+            return utilPath + "dates/nearest"
+        }
+    }
+    
+    var parameter: [String : Any]? {
+        switch self {
+        case .getFilteredDateCourse(let sortBy):
+            return ["sortBy" : sortBy]
+        default:
+            return .none
+        }
+    }
+    
+    var task: Task {
+        if let parameter = parameter {
+            return .requestParameters(parameters: parameter, encoding: URLEncoding.default)
+        } else {
+            return .requestPlain
+        }
+    }
+    
+    var headers: [String : String]? {
+        let headers = ["Content-Type" : "application/json",
+                       "Authorization" : "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI0IiwiaWF0IjoxNzIxMDY2MDA2LCJleHAiOjE3MjM0ODUxMDZ9.-3xRrDlDixBsHKH7sUMjcYkHT8ry5f3STuwmL27abF8"]
+        return headers
+    }
+    
+}
