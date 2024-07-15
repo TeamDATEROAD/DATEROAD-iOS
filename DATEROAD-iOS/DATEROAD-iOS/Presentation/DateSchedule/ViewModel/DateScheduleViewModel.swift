57f73706ba5ddc2f9f0908fb1df51010cd637ba9
cirtuare <cirtuare@gmail.com>
2024-07-16 00:31:48 +0900

    [Fix] 잘못된 더미데이터 해결 (#64)

--- DATEROAD-iOS/DATEROAD-iOS/Presentation/DateSchedule/ViewModel/DateScheduleViewModel.swift
+++ DATEROAD-iOS/DATEROAD-iOS/Presentation/DateSchedule/ViewModel/DateScheduleViewModel.swift
@@ -23,9 +23,10 @@ class DateScheduleViewModel {
     
     var pastDateScheduleDummyData = DateScheduleModel(
         dateCards: [
-            DateCardModel(courseID: 1, dateCalendar: "June 24", dDay: 3, tags: ["🎨 전시·팝업", "🎨 전시·팝업", "🎨 전시·팝업"], dateTitle: "성수동 당일치기 데이트 가볼까요?", dateLocation: "건대/성수/왕십리"),
-            DateCardModel(courseID: 2, dateCalendar: "June 24", dDay: 3, tags: ["🎨 전시·팝업", "🎨 전시·팝업", "🎨 전시·팝업"], dateTitle: "성수동 당일치기 데이트 가볼까요?", dateLocation: "건대/성수/왕십리"),
-            DateCardModel(courseID: 3, dateCalendar: "June 24", dDay: 3, tags: ["🎨 전시·팝업", "🎨 전시·팝업", "🎨 전시·팝업"], dateTitle: "성수동 당일치기 데이트 가볼까요?", dateLocation: "건대/성수/왕십리")
+            DateCardModel(dateID: 1, title: "성수동 당일치기 데이트 가볼까요?", date: "June 24", city: "건대/성수/왕십리", tags: ["🎨 전시·팝업", "🎨 전시·팝업", "🎨 전시·팝업"], dDay: 3),
+            DateCardModel(dateID: 2, title: "성수동 당일치기 데이트 가볼까요?", date: "June 24", city: "건대/성수/왕십리", tags: ["🎨 전시·팝업", "🎨 전시·팝업", "🎨 전시·팝업"], dDay: 3),
+            DateCardModel(dateID: 3, title: "성수동 당일치기 데이트 가볼까요?", date: "June 24", city: "건대/성수/왕십리", tags: ["🎨 전시·팝업", "🎨 전시·팝업", "🎨 전시·팝업"], dDay: 3),
+            DateCardModel(dateID: 4, title: "성수동 당일치기 데이트 가볼까요?", date: "June 24", city: "건대/성수/왕십리", tags: ["🎨 전시·팝업", "🎨 전시·팝업", "🎨 전시·팝업"], dDay: 3)
         ]
     )
     
