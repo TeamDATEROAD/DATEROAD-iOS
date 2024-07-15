cd8b8c5507546c910d709a0ae0078d01fec7e942
seuriseuljjeok <heeseul09@naver.com>
2024-07-16 05:12:47 +0900

    [Feat] 배너 서비스 구현 (#74)

--- DATEROAD-iOS/DATEROAD-iOS/Network/Main/MainService.swift
+++ DATEROAD-iOS/DATEROAD-iOS/Network/Main/MainService.swift
@@ -12,6 +12,8 @@ import Moya
 protocol MainServiceProtocol {
     func getUserProfile(
                         completion: @escaping (NetworkResult<GetUserProfileResponse>) -> ())
+    func getBanner(
+                   completion: @escaping (NetworkResult<GetBannerResponse>) -> ())
     func getUpcomingDate(completion: @escaping (NetworkResult<GetUpcomingDateResponse>) -> ())
 }
 
@@ -24,6 +26,19 @@ final class MainService: BaseService, MainServiceProtocol {
             switch result {
             case .success(let response):
                 let networkResult: NetworkResult<GetUserProfileResponse> = self.judgeStatus(statusCode: response.statusCode, data: response.data) 
+                completion(networkResult)
+            case .failure(let err):
+                print(err)
+            }
+        }
+    }
+    
+    
+    func getBanner(completion: @escaping (NetworkResult<GetBannerResponse>) -> ()) {
+        provider.request(.getBanner) { result in
+            switch result {
+            case .success(let response):
+                let networkResult: NetworkResult<GetBannerResponse> = self.judgeStatus(statusCode: response.statusCode, data: response.data)
                 completion(networkResult)
             case .failure(let err):
                 print(err)
