9a06d92b2886f85e85b4563484cde619590f5dfd
seuriseuljjeok <heeseul09@naver.com>
2024-07-16 05:15:11 +0900

    [Feat] 필터링 코스 response DTO 구현 (#74)

--- DATEROAD-iOS/DATEROAD-iOS/Network/Main/DTO/GetFilteredDateCourseResponse.swift
+++ DATEROAD-iOS/DATEROAD-iOS/Network/Main/DTO/GetFilteredDateCourseResponse.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+struct GetFilteredDateCourseResponse: Codable {
+    let courses: [Course]
+}
+
+struct Course: Codable {
+    let courseID: Int
+    let thumbnail, title, city: String
+    let like, cost, duration: Int
+
+    enum CodingKeys: String, CodingKey {
+        case courseID = "courseId"
+        case thumbnail, title, city, like, cost, duration
+    }
+}
