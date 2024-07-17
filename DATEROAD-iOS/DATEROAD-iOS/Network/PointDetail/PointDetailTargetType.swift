964e1fec42da9e0f7ad1bad18e0e6b26f6c381a7
cirtuare <cirtuare@gmail.com>
2024-07-17 21:29:32 +0900

    [Add] 서비스 파일 추가 (#85)

---
+++ DATEROAD-iOS/DATEROAD-iOS/Network/PointDetail/PointDetailTargetType.swift
@@ -0,0 +1,20 @@
+//
+//  PointDetailTargetType.swift
+//  DATEROAD-iOS
+//
+//  Created by 이수민 on 7/17/24.
+//
+
+import UIKit
+
+class PointDetailTargetType: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
