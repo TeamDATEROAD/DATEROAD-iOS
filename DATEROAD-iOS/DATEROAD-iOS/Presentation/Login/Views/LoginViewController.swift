977f243adad9774853f7e45da136b5c8f0f25b8c
seuriseuljjeok <heeseul09@naver.com>
2024-07-18 04:33:48 +0900

    [Feat] 로그인 분기처리 구현 (#80)

--- DATEROAD-iOS/DATEROAD-iOS/Presentation/Login/Views/LoginViewController.swift
+++ DATEROAD-iOS/DATEROAD-iOS/Presentation/Login/Views/LoginViewController.swift
@@ -61,9 +61,14 @@ extension LoginViewController {
     
     // TODO: - 추후 뷰컨 변경 예정
     
-    func pushToPointSystemManualVC() {
-        let pointSystemManualVC = OnboardingViewController()
-        self.navigationController?.pushViewController(pointSystemManualVC, animated: true)
+    func pushToNextVC(isSignIn: Bool) {
+        if isSignIn {
+            let mainVC = TabBarController()
+            self.navigationController?.pushViewController(mainVC, animated: true)
+        } else {
+            let pointSystemManualVC = OnboardingViewController()
+            self.navigationController?.pushViewController(pointSystemManualVC, animated: true)
+        }
     }
     
 }
