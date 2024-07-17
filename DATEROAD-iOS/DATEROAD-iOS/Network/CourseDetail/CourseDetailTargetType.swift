7012e37990e09d782b786d38af1b8e73bccbf932
kms0233 <cnblue789@naver.com>
2024-07-17 21:53:53 +0900

    [Setting] 기본 네트워크 폴더링 (#82)

---
+++ DATEROAD-iOS/DATEROAD-iOS/Network/CourseDetail/CourseDetailTargetType.swift
@@ -0,0 +1,8 @@
+//
+//  CourseDetailTargetType.swift
+//  DATEROAD-iOS
+//
+//  Created by 김민서 on 7/17/24.
+//
+
+import Foundation
---
+++ DATEROAD-iOS/DATEROAD-iOS/Network/CourseDetail/DTO/GetCourseDetailResponse.swift
@@ -0,0 +1,8 @@
+//
+//  GetCourseDetailResponse.swift
+//  DATEROAD-iOS
+//
+//  Created by 김민서 on 7/17/24.
+//
+
+import Foundation
