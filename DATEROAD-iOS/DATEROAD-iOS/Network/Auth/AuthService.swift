4cfb1d5692f9a194b8d1ef15ac9d99e93123897d
seuriseuljjeok <heeseul09@naver.com>
2024-07-17 23:25:26 +0900

    [Feat] 회원가입 서비스 구현 (#80)

--- DATEROAD-iOS/DATEROAD-iOS/Network/Auth/AuthService.swift
+++ DATEROAD-iOS/DATEROAD-iOS/Network/Auth/AuthService.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+import Moya
+
+protocol AuthServiceProtocol {
+    func postSignUp(requestBody: PostSignUpRequest, completion: @escaping (NetworkResult<PostSignUpResponse>) -> ())
+}
+
+final class AuthService: BaseService, AuthServiceProtocol {
+    let provider = MoyaProvider<AuthTargetType>(plugins: [MoyaLoggingPlugin()])
+
+    func postSignUp(requestBody : PostSignUpRequest, completion: @escaping (NetworkResult<PostSignUpResponse>) -> ()) {
+        provider.request(.postSignUp(requestBody: requestBody)) { result in
+            switch result {
+            case .success(let response):
+                let networkResult: NetworkResult<PostSignUpResponse> = self.judgeStatus(statusCode: response.statusCode, data: response.data)
+                completion(networkResult)
+            case .failure(let err):
+                print(err)
+            }
+        }
+    }
+}
