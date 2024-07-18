c2d677237dad85358850c58c12ae369453ec5e6c
seuriseuljjeok <heeseul09@naver.com>
2024-07-18 09:19:55 +0900

    [Feat] 유저 서비스 구현 (#80)

--- DATEROAD-iOS/DATEROAD-iOS/Network/User/UserService.swift
+++ DATEROAD-iOS/DATEROAD-iOS/Network/User/UserService.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+import Moya
+
+protocol UserServiceProtocol {
+    func getUserProfile(completion: @escaping (NetworkResult<GetUserProfileResponse>) -> ())
+}
+
+final class UserService: BaseService, UserServiceProtocol {
+    
+    let provider = MoyaProvider<UserTargetType>(plugins: [MoyaLoggingPlugin()])
+    
+    func getUserProfile(completion: @escaping (NetworkResult<GetUserProfileResponse>) -> ()) {
+        provider.request(.getUserProfile) { result in
+            switch result {
+            case .success(let response):
+                let networkResult: NetworkResult<GetUserProfileResponse> = self.judgeStatus(statusCode: response.statusCode, data: response.data)
+                completion(networkResult)
+            case .failure(let err):
+                print(err)
+            }
+        }
+    }
+}
