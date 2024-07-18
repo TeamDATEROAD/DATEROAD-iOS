8e352ea47d278caacf51f81424c1814c20d3fa83
seuriseuljjeok <heeseul09@naver.com>
2024-07-18 09:20:11 +0900

    [Feat] 유저 TargetType 구현 (#80)

--- DATEROAD-iOS/DATEROAD-iOS/Network/User/UserTargetType.swift
+++ DATEROAD-iOS/DATEROAD-iOS/Network/User/UserTargetType.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+
+import Moya
+
+enum UserTargetType {
+    case getUserProfile
+}
+
+extension UserTargetType: BaseTargetType {
+
+    var utilPath: String {
+        return "api/v1/"
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .getUserProfile:
+            return .get
+        }
+    }
+    
+    var path: String {
+        switch self {
+        case .getUserProfile:
+            return utilPath + "users"
+        }
+    }
+
+    var task: Task {
+        return .requestPlain
+    }
+    
+    var headers: [String : String]? {
+        let token = UserDefaults.standard.string(forKey: "accessToken") ?? ""
+        let headers = ["Content-Type" : "application/json", "Authorization" : "Bearer " + token]
+        return headers
+    }
+}
