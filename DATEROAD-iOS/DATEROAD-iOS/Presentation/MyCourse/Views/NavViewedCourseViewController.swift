fea23e0a305d8a18ee27b0ca323e7d804727e206
seuriseuljjeok <heeseul09@naver.com>
2024-07-20 12:29:34 +0900

    [Fix] 열람한 코스 데이터 불러오는 시점 수정 (#200)

--- DATEROAD-iOS/DATEROAD-iOS/Presentation/MyCourse/Views/NavViewedCourseViewController.swift
+++ DATEROAD-iOS/DATEROAD-iOS/Presentation/MyCourse/Views/NavViewedCourseViewController.swift
@@ -33,6 +33,11 @@ class NavViewedCourseViewController: BaseNavBarViewController {
         setEmptyView()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+           viewedCourseViewModel.setViewedCourseData()
+       }
+       
+    
     override func setHierarchy() {
         super.setHierarchy()
         
