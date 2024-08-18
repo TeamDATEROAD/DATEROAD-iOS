c98411355b60cf48c6dc877c0e3d85c20518da64
cirtuare <cirtuare@gmail.com>
2024-08-18 20:14:22 +0900

    [Fix] 지난 데이트 날짜 표기 형식 오류 수정 (#163)

--- DATEROAD-iOS/DATEROAD-iOS/Presentation/DateSchedule/ViewModel/DateScheduleViewModel.swift
+++ DATEROAD-iOS/DATEROAD-iOS/Presentation/DateSchedule/ViewModel/DateScheduleViewModel.swift
@@ -39,7 +39,7 @@ class DateScheduleViewModel: Serviceable {
                     let tagsModel: [TagsModel] = date.tags.map { tag in
                         TagsModel(tag: tag.tag)
                     }
-                    return DateCardModel(dateID: date.dateID, title: date.title, date: date.date, city: date.city, tags: tagsModel, dDay: date.dDay)
+                    return DateCardModel(dateID: date.dateID, title: date.title, date: date.date.formatDateFromString(inputFormat: "yyyy.MM.dd", outputFormat: "yyyy년 M월 d일") ?? "", city: date.city, tags: tagsModel, dDay: date.dDay)
                 }
                 
                 self.pastDateScheduleData.value = dateScheduleInfo
