9b23285e66bc157bc0c9c103b348c9e08aa275c5
kms0233 <cnblue789@naver.com>
2024-09-11 22:52:28 +0900

    [Refactor] compositional Layout 함수화 (#198)

---
+++ DATEROAD-iOS/DATEROAD-iOS/Global/UIComponents/DRCompositionalLayoutFactory.swift
@@ -0,0 +1,77 @@
+//
+//  DRCompositionalLayoutFactory.swift
+//  DATEROAD-iOS
+//
+//  Created by 김민서 on 9/11/24.
+//
+
+import UIKit
+
+enum SupplementaryItemType {
+    case header
+    case footer
+}
+
+// MARK: - Compositional Layout
+
+class DRCompositionalLayoutFactory {
+    
+    /// Item 생성 시 사용합니다.
+    func createItem(
+        widthDimension: NSCollectionLayoutDimension,
+        heightDimension: NSCollectionLayoutDimension,
+        contentInsets: NSDirectionalEdgeInsets = .zero
+    ) -> NSCollectionLayoutItem {
+        let itemSize = NSCollectionLayoutSize(widthDimension: widthDimension, heightDimension: heightDimension)
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = contentInsets
+        return item
+    }
+    
+    /// Group 생성 시 사용합니다.
+    func createGroup(
+        item: [NSCollectionLayoutItem],
+        widthDimension: NSCollectionLayoutDimension,
+        heightDimension: NSCollectionLayoutDimension,
+        contentInsets: NSDirectionalEdgeInsets = .zero
+    ) -> NSCollectionLayoutGroup {
+        let groupSize = NSCollectionLayoutSize(widthDimension: widthDimension, heightDimension: heightDimension)
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: item)
+        group.contentInsets = contentInsets
+        return group
+    }
+    
+    /// Header 또는 Footer 생성 시 사용합니다.
+    /// - type에 .header 또는 .footer를 작성
+    func createBoundarySupplementaryItem(
+        type: SupplementaryItemType,
+        widthDimension: NSCollectionLayoutDimension,
+        heightDimension: NSCollectionLayoutDimension,
+        alignment: NSRectAlignment = .top
+    ) -> NSCollectionLayoutBoundarySupplementaryItem {
+        return NSCollectionLayoutBoundarySupplementaryItem(
+            layoutSize: .init(
+                widthDimension: widthDimension,
+                heightDimension: heightDimension
+            ),
+            elementKind: type == .header ? UICollectionView.elementKindSectionHeader : UICollectionView.elementKindSectionFooter,
+            alignment: alignment
+        )
+    }
+    
+    /// Section 생성 시 사용합니다.
+    func createLayoutSection(
+        group: NSCollectionLayoutGroup,
+        orthogonalScrollingBehavior: UICollectionLayoutSectionOrthogonalScrollingBehavior = .none,
+        sectionContentInsets: NSDirectionalEdgeInsets = .zero,
+        boundarySupplementaryItems: [NSCollectionLayoutBoundarySupplementaryItem]? = nil
+    ) -> NSCollectionLayoutSection {
+        let section = NSCollectionLayoutSection(group: group)
+        section.orthogonalScrollingBehavior = orthogonalScrollingBehavior
+        section.contentInsets = sectionContentInsets
+        if let supplementaryItems = boundarySupplementaryItems {
+            section.boundarySupplementaryItems = supplementaryItems
+        }
+        return section
+    }
+}
