1d285211481e3daf47fac0fffc2ebfa604aa9441
seuriseuljjeok <heeseul09@naver.com>
2024-11-08 00:03:00 +0900

    [Fix] '내가 열람한 코스' 이미지 로딩 중 버벅임 이슈 해결 (#268)

--- DATEROAD-iOS/DATEROAD-iOS/Presentation/MyCourse/Views/ViewedCourseViewController.swift
+++ DATEROAD-iOS/DATEROAD-iOS/Presentation/MyCourse/Views/ViewedCourseViewController.swift
@@ -190,11 +190,7 @@ extension ViewedCourseViewController {
         self.viewedCourseViewModel.viewedCoursesModelIsUpdate.bind { [weak self] flag in
             guard let flag else { return }
             if flag {
-                DispatchQueue.main.async {
-                    self?.viewedCourseView.myCourseListCollectionView.performBatchUpdates({
-                        self?.viewedCourseView.myCourseListCollectionView.reloadSections(IndexSet(integer: 0))
-                    })
-                }
+                self?.viewedCourseView.myCourseListCollectionView.reloadSections(IndexSet(integer: 0))
                 self?.viewedCourseViewModel.viewedCoursesModelIsUpdate.value = false
             }
         }
