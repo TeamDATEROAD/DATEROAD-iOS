52127c8d1c1741e0294c193e1d217c0f8b1c372a
ParkSY0919 <qhr498@gmail.com>
2025-02-05 22:58:21 +0900

    [Chore] PostAddScheduleRequest2 임시 추가 (#282)

--- DATEROAD-iOS/DATEROAD-iOS/Network/AddSchedule/DTO/PostAddScheduleRequest.swift
+++ DATEROAD-iOS/DATEROAD-iOS/Network/AddSchedule/DTO/PostAddScheduleRequest.swift
@@ -7,6 +7,21 @@
 
 import Foundation
 
+struct PostAddScheduleRequest2: Codable {
+    
+    var dateName: String = ""
+    var date: String = ""
+    var startAt: String = ""
+    
+    var tags: [PostAddScheduleTag] = []
+    
+    var country: String = ""
+    var city: String = ""
+    
+    var places: [PostAddSchedulePlace] = []
+    
+}
+
 // MARK: - PostAddScheduleRequest
 
 struct PostAddScheduleRequest: Codable {
