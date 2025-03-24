83f1be16d9a8d9c46ad449e9d62408573797eef2
cirtuare <cirtuare@gmail.com>
2025-03-24 11:46:37 +0900

    [Fix] NavType.tab StringLiterals로 분리 (#288)

--- DATEROAD-iOS/DATEROAD-iOS/Presentation/MyCourse/Views/ViewedCourseViewController.swift
+++ DATEROAD-iOS/DATEROAD-iOS/Presentation/MyCourse/Views/ViewedCourseViewController.swift
@@ -24,7 +24,7 @@ final class ViewedCourseViewController: BaseViewController {
     
     private let arrowButton = UIButton()
     
-    private var viewedCourseView = MyCourseListView(type: "tab")
+    private var viewedCourseView = MyCourseListView(type: StringLiterals.NavType.tab)
     
     private let errorView: DRErrorViewController = DRErrorViewController()
     
