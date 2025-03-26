3b4f69b080336e4bdffa635076863f2dd7eeb068
seuriseuljjeok <heeseul09@naver.com>
2025-03-27 01:29:21 +0900

    [Feat] 텍스트 라벨 공용 컴포넌트  '마이페이지' 적용 (#289)

--- DATEROAD-iOS/DATEROAD-iOS/Presentation/MyPage/Views/Cells/MyPageTableViewCell.swift
+++ DATEROAD-iOS/DATEROAD-iOS/Presentation/MyPage/Views/Cells/MyPageTableViewCell.swift
@@ -11,7 +11,7 @@ final class MyPageTableViewCell: BaseTableViewCell {
     
     // MARK: - UI Properties
     
-    private let titleLabel: UILabel = UILabel()
+    private let titleLabel: DRTextLabel = DRTextLabel(textLabelType: .clear(.semi15_black), alignment: .left)
     
     let rightArrowButton: DRImageButton = DRImageButton(image: UIImage(resource: .arrowRightLarge), buttonName: .white_gray400_0)
     
@@ -32,12 +32,6 @@ final class MyPageTableViewCell: BaseTableViewCell {
             $0.trailing.equalToSuperview().inset(23)
             $0.centerY.equalToSuperview()
         }
-    }
-    
-    override func setStyle() {
-        titleLabel.setLabel(alignment: .left,
-                        textColor: UIColor(resource: .drBlack),
-                        font: UIFont.suit(.body_semi_15))
     }
     
 }
