70227d4970c9da7aedc8b3b763b6ff73640e5a90
cirtuare <cirtuare@gmail.com>
2025-03-27 17:24:51 +0900

    [Chore] 코드리뷰 반영 - 줄바꿈 (#288)

--- DATEROAD-iOS/DATEROAD-iOS/Global/Utils/Enums/DateCardType.swift
+++ DATEROAD-iOS/DATEROAD-iOS/Global/Utils/Enums/DateCardType.swift
@@ -17,41 +17,56 @@ enum DateCardType: Int, CaseIterable {
     
     var bgColor: UIColor {
         switch self {
-        case .pink: return UIColor(resource: .pink200)
-        case .purple: return UIColor(resource: .purple200)
-        case .lime: return UIColor(resource: .lime200)
+        case .pink:
+            return UIColor(resource: .pink200)
+        case .purple:
+            return UIColor(resource: .purple200)
+        case .lime:
+            return UIColor(resource: .lime200)
         }
     }
     
     var buttonColor: UIColor {
         switch self {
-        case .pink: return UIColor(resource: .pink100)
-        case .purple: return UIColor(resource: .purple100)
-        case .lime: return UIColor(resource: .lime100)
+        case .pink:
+            return UIColor(resource: .pink100)
+        case .purple:
+            return UIColor(resource: .purple100)
+        case .lime:
+            return UIColor(resource: .lime100)
         }
     }
     
     var topImage: UIImage {
         switch self {
-        case .pink: return UIImage(resource: .lilacTop)
-        case .purple: return UIImage(resource: .deepPurpleTop)
-        case .lime: return UIImage(resource: .limeTop)
+        case .pink:
+            return UIImage(resource: .lilacTop)
+        case .purple:
+            return UIImage(resource: .deepPurpleTop)
+        case .lime:
+            return UIImage(resource: .limeTop)
         }
     }
     
     var bottomImage: UIImage {
         switch self {
-        case .pink: return UIImage(resource: .lilacBottom)
-        case .purple: return UIImage(resource: .deepPurpleBottom)
-        case .lime: return UIImage(resource: .limeBottom)
+        case .pink:
+            return UIImage(resource: .lilacBottom)
+        case .purple:
+            return UIImage(resource: .deepPurpleBottom)
+        case .lime:
+            return UIImage(resource: .limeBottom)
         }
     }
     
     var ribbonImage: UIImage {
         switch self {
-        case .pink: return UIImage(resource: .lilacRibbon)
-        case .purple: return UIImage(resource: .deepPurpleRibbon)
-        case .lime: return UIImage(resource: .limeRibbon)
+        case .pink:
+            return UIImage(resource: .lilacRibbon)
+        case .purple:
+            return UIImage(resource: .deepPurpleRibbon)
+        case .lime:
+            return UIImage(resource: .limeRibbon)
         }
     }
     
