45c5f51153652683426f8ed322d8fd065c03d232
seuriseuljjeok <heeseul09@naver.com>
2025-04-09 15:22:43 +0900

    [Fix] '지난 데이트 상세' - 하단 버튼 안보임 이슈 해결 + 레이아웃 수정 (#299)

--- DATEROAD-iOS/DATEROAD-iOS/Presentation/DateSchedule/View/PastDateDetailViewController.swift
+++ DATEROAD-iOS/DATEROAD-iOS/Presentation/DateSchedule/View/PastDateDetailViewController.swift
@@ -239,9 +239,7 @@ extension PastDateDetailViewController {
     
     private func setButton() {
         pastDateDetailContentView.kakaoShareButton.isHidden = true
-        
-        // TODO: - 코스 공유 버튼 확정 시 courseShareButton.isHidden = false로 변경
-        pastDateDetailContentView.courseShareButton.isHidden = true
+        pastDateDetailContentView.courseShareButton.isHidden = false
         
         pastDateDetailContentView.courseShareButton.addTarget(self, action: #selector(tapShareCourse), for: .touchUpInside)
     }
