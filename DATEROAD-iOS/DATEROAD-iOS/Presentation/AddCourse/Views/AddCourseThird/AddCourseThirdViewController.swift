6d70ef1f62404bf25ce184763454ad78a9820b9a
ParkSY0919 <qhr498@gmail.com>
2025-04-16 13:37:21 +0900

    [Fix] 뷰3 가격 numberFormatted 실시간 적용 (#310)

--- DATEROAD-iOS/DATEROAD-iOS/Presentation/AddCourse/Views/AddCourseThird/AddCourseThirdViewController.swift
+++ DATEROAD-iOS/DATEROAD-iOS/Presentation/AddCourse/Views/AddCourseThird/AddCourseThirdViewController.swift
@@ -291,9 +291,10 @@ extension AddCourseThirdViewController: UITextViewDelegate {
 
 extension AddCourseThirdViewController: UITextFieldDelegate {
     
-    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+    func textFieldDidChangeSelection(_ textField: UITextField) {
         textField.font = UIFont.suit(.body_med_13)
-        return true
+        let money = textField.text?.filter { $0.isNumber }
+        viewModel.priceText.value = Int(money ?? "0")
     }
     
     func textFieldDidBeginEditing(_ textField: UITextField) {
@@ -318,9 +319,9 @@ extension AddCourseThirdViewController: UITextFieldDelegate {
             }
             self.view.layoutIfNeeded() // 레이아웃 즉시 갱신
         }
-        // textField 값이 변경된 경우 ',' 제거한 값으로 처리
-        let money = textField.text?.filter { $0.isNumber }
-        viewModel.priceText.value = Int(money ?? "0")
+//        // textField 값이 변경된 경우 ',' 제거한 값으로 처리
+//        let money = textField.text?.filter { $0.isNumber }
+//        viewModel.priceText.value = Int(money ?? "0")
     }
     
 }
