01c199cc656d3a216b9c30ee0037b766652ecdb2
seuriseuljjeok <heeseul09@naver.com>
2025-04-17 16:55:22 +0900

    [Fix] 카카오 로그인 글씨 센터 조정 (#309)

--- DATEROAD-iOS/DATEROAD-iOS/Global/Protocols/DRButtonType.swift
+++ DATEROAD-iOS/DATEROAD-iOS/Global/Protocols/DRButtonType.swift
@@ -73,26 +73,6 @@ struct DisabledButton: DRButtonType {
     
 }
 
-struct KakaoLoginButton: DRButtonType {
-    
-    var bgColor: UIColor = UIColor(resource: .kakaoBg)
-    
-    var fontColor: UIColor = UIColor(resource: .drBlack).withAlphaComponent(0.85)
-    
-    var font: UIFont = UIFont.systemFont(ofSize: 15, weight: .semibold)
-    
-}
-
-struct AppleLoginButton: DRButtonType {
-    
-    var bgColor: UIColor = UIColor(resource: .drBlack)
-    
-    var fontColor: UIColor = UIColor(resource: .drWhite)
-    
-    var font: UIFont = UIFont.suit(.body_bold_15)
-    
-}
-
 struct UnselectedButton: DRButtonType {
     
     var bgColor: UIColor = UIColor(resource: .gray100)
