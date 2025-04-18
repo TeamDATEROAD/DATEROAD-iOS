cbc732b9fbce3481677068942ba043dbcb55620c
ParkSY0919 <qhr498@gmail.com>
2025-04-18 16:37:34 +0900

    [Fix] modal descriptionLabel 레이아웃 수정 등 (#310)

--- DATEROAD-iOS/DATEROAD-iOS/Presentation/AddCourse/Views/AddCourseThird/AddCourseThirdViewController.swift
+++ DATEROAD-iOS/DATEROAD-iOS/Presentation/AddCourse/Views/AddCourseThird/AddCourseThirdViewController.swift
@@ -194,7 +194,7 @@ private extension AddCourseThirdViewController {
     func successDone() {
         let customAlertVC = DRCustomAlertViewController(
             rightActionType: .none,
-            alertTextType: .noDescription,
+            alertTextType: .hasDecription,
             titleText: StringLiterals.AddCourseOrSchedule.AddCourseAlert.alertTitleLabel,
             descriptionText: StringLiterals.AddCourseOrSchedule.AddCourseAlert.alertSubTitleLabel,
             longButton: DRTextButton(title: StringLiterals.AddCourseOrSchedule.AddCourseAlert.doneButton, buttonName: .bold_purple_10)
